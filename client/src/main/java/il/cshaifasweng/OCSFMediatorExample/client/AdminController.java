c15928d82c0c840b790988698cb77bf36c09c79f
Or Barer <or.barer@gmail.com>
2025-08-24 18:00:12 +0300

    admin controller fix

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/AdminController.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/AdminController.java
@@ -57,7 +57,7 @@ public class AdminController implements Initializable {
     private TextField phoneField;
     
     @FXML
-    private TextField addressField;
+    private TextArea addressField;
     
     @FXML
     private TextField departmentField;
